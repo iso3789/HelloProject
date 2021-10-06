0ddbea8064a25982f1dedd1c7b5dad69f3e1a470
Ivan Dzido <ivan.dzido@bulme.at>
2021-10-06 08:56:13 +0200

    date

--- hello.py
+++ hello.py
@@ -4,3 +4,6 @@ print("Guten Start ins neue Schuljahr!")
 name = input("Bitte tragen Sie Ihren Namen ein: ")
 print(f"Guten Tag {name}!")
 
+import datetime
+x = datetime.datetime.now()
+print(f"Heute ist der {x}!")
