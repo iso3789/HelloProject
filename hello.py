4c4954aa186ca70177c8599fe5a0222269e80532
Ivan Dzido <ivan.dzido@bulme.at>
2021-10-13 08:59:44 +0200

    author

--- hello.py
+++ hello.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
-
+# (c) ivan.dzido@bulme.at
 print("Guten Start ins neue Schuljahr!")
-<<<<<<< HEAD
 name = input("Bitte tragen Sie Ihren Namen ein: ")
 print(f"Guten Tag {name}!")
-
 import datetime
 x = datetime.datetime.now()
 print(f"Heute ist der {x}!")
-print("----------------------------------------------")
-=======
 print("See you later")
+print("----------------------------------------------")
